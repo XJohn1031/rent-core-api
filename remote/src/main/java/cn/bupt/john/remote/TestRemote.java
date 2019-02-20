3f94bbdf1a42f83963bf02410f9602deeb63b57b
XJohn <xj124116211@qq.com>
2019-02-20 20:16:27 +0800

    add all

---
+++ remote/src/main/java/cn/bupt/john/remote/TestRemote.java
@@ -0,0 +1,16 @@
+package cn.bupt.john.remote;
+
+/**
+ * 测试remote接口
+ * @author xjohn
+ * @since 20 February 2019
+ */
+public interface TestRemote {
+	/**
+	 * 测试remote方法
+	 * @param request
+	 * @return
+	 */
+	String testRemoteMethod(String request);
+
+}
